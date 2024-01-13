b6a23fc9ffcafbca9a8ba9b4e7614d30c85f6b0f
Mario Petkovski <mario.petkovski@teaming.inteligenta.io>
2024-01-13 05:58:16 +0100

    added files for production

--- insta/settings.py
+++ insta/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z6efeo62^2q1d0kh-1s-4t@3&!48w4p8u4iw&ieewx_af32%dz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
 
 
 # Application definition
